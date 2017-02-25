a050125ba0311609475543b30f3ea75ad386f284
Ryosuke Sato <rskjtwp@gmail.com>
2017-02-25 22:37:30 +0900

    Cimponents and Props refs: https://facebook.github.io/react/docs/components-and-props.html

--- javascript/tutorial-react/src/index.js
+++ javascript/tutorial-react/src/index.js
@@ -2,17 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, World</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
   );
 }
 
-setInterval(tick, 1000);
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React',
+  author: {
+    name: 'ryo-san470',
+    avatarUrl: 'https://avatars1.githubusercontent.com/u/5006625?v=3&u=eff862d174cf6c3338ff213c60419743230f9544&s=400'
+  }
+};
+
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />,
+  document.getElementById('root')
+);
+
