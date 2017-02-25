c125c2abbf0f85201bbec37f900061cb90d3c2a5
Ryosuke Sato <rskjtwp@gmail.com>
2017-02-25 22:02:26 +0900

    [javascript] 時計の表示

--- javascript/tutorial-react/src/index.js
+++ javascript/tutorial-react/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
