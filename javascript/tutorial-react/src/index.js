938b50707920b07264525feb60f4f677f0dc20ff
Ryosuke SATO <rskjtwp@gmail.com>
2017-03-22 17:39:58 +0900

    [React] List and Keys - Basic List Component

--- javascript/tutorial-react/src/index.js
+++ javascript/tutorial-react/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 const numbers = [1, 2, 3, 4, 5];
-// Rendering Multiple Components
-const listItems = numbers.map((number) =>
-  <li>{number}</li>
-);
-
-
 ReactDOM.render(
-  <ul>{listItems}</ul>,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
