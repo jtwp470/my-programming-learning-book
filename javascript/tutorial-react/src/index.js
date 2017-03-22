d079e67e92cea2a7876b59ea5e5d756eac58c6e0
Ryosuke SATO <rskjtwp@gmail.com>
2017-03-22 17:30:18 +0900

    [React] Lists and Key - Rendering Multiple Components

--- javascript/tutorial-react/src/index.js
+++ javascript/tutorial-react/src/index.js
@@ -1,76 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
-
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-
-    let button = null;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        <div>The user is <b>{isLoggedIn ? 'currently' : 'not'}</b> logged in.</div>
-        {button}
-      </div>
-    );
-  }
-}
+const numbers = [1, 2, 3, 4, 5];
+// Rendering Multiple Components
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
 
 
 ReactDOM.render(
-  <LoginControl />,
+  <ul>{listItems}</ul>,
   document.getElementById('root')
 );
